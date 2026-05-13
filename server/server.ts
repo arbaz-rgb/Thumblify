750711219ee83af215a09eb857c124bc7687fc36
Arbaz Khan <ak2208803@gmail.com>
2026-05-13 12:56:27 +0530

    add the userRoutes

--- server/server.ts
+++ server/server.ts
@@ -6,6 +6,7 @@ import session from "express-session";
 import MongoStore from "connect-mongo";
 import AuthRouter from "./routes/AuthRoutes.js";
 import ThumbnailRouter from "./routes/ThumbnailRoutes.js";
+import UserRouter from "./routes/UserRoutes.js";
 
 declare module "express-session" {
   interface SessionData {
@@ -48,6 +49,7 @@ app.get("/", (req: Request, res: Response) => {
 });
 app.use("/api/auth", AuthRouter);
 app.use("/api/thumbnail", ThumbnailRouter);
+app.use("api/user", UserRouter);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
