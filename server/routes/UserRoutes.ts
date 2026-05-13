f694b581e3501b3cdbb9e713d86f6e4f9131a481
Arbaz Khan <ak2208803@gmail.com>
2026-05-14 01:13:16 +0530

    protect the route

--- server/routes/UserRoutes.ts
+++ server/routes/UserRoutes.ts
@@ -3,10 +3,11 @@ import {
   getThumbnailbyId,
   getUsersThumbnails,
 } from "../controllers/UserController.js";
+import protect from "../middleware/auth.js";
 
 const UserRouter = express.Router();
 
-UserRouter.get("/thumbnails", getUsersThumbnails);
-UserRouter.get("/thumbnail/:id", getThumbnailbyId);
+UserRouter.get("/thumbnails", protect, getUsersThumbnails);
+UserRouter.get("/thumbnail/:id", protect, getThumbnailbyId);
 
 export default UserRouter;
